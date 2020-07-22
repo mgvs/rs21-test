4eeaf5582472f3d56f1bc5162ff21265803268d0
well2life <well2life@gmail.com>
2020-07-22 15:42:52 +0300

    Added FacebookAllPlaces

--- rs21_test/app/handlers/facebook.py
+++ rs21_test/app/handlers/facebook.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import csv
-import datetime
 import re
 
 from aiohttp import web
@@ -87,4 +85,50 @@ class FacebookHandler(web.View):
 
         result = await self.request.app._db.facebook.find(filter_query, {'_id': 0}).to_list(length=None)
         return web.json_response(result, dumps=json_dumps)
+
+
+class FacebookAllPlacesHandler(web.View):
+    """Facebook All Places handler"""
+
+    async def get(self) -> web.Response:
+        """
+        ---
+        summary: 'Get list of all Facebook places'
+        tags:
+          - FacebookAllPlaces
+        responses:
+          '200':
+            description: 'Return list of all Facebook places'
+        """
+
+        place = self.request.rel_url.query.get('query', None)
+        place_type = self.request.rel_url.query.get('type', None)
+        lat = self.request.rel_url.query.get('lat', None)
+        lon = self.request.rel_url.query.get('lon', None)
+        dist = int(self.request.rel_url.query.get('dist', 100))
+
+        filter_query = {}
+
+        if place:
+            q_str = re.compile(r'^.*?{}.*?$'.format(place.replace(' ', '\s+')), re.IGNORECASE)
+            filter_query.update({'place': q_str})
+
+        if place_type:
+            q_str = re.compile(r'^.*?{}.*?$'.format(place_type.replace(' ', '\s+')), re.IGNORECASE)
+            filter_query.update({'type': q_str})
+
+        if all([lon, lat]):
+            filter_query.update({
+                'location': {
+                    '$near': {
+                        '$geometry': {
+                           'type': "Point",
+                            'coordinates': [float(lon), float(lat)]},
+                        '$maxDistance': dist
+                    }
+                }
+            })  # 100 meters
+
+        result = await self.request.app._db.facebook.find({}, {'_id': 0}).to_list(length=None)
+        return web.json_response({"all_places": {i.get('place') for i in result}}, dumps=json_dumps)
 
